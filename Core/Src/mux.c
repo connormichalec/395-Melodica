9bee4f3661cc1dbea879bb17f29f9696fefed6af
KobeDuda <kobesduda@gmail.com>
2025-03-05 00:01:01 -0600

    32-note functionality
    Got all 32 notes working

--- Core/Src/mux.c
+++ Core/Src/mux.c
@@ -14,11 +14,26 @@ uint8_t ReadNote(uint8_t key) {
 
 
 void ReadKeyboard(uint8_t status[]) {
+	GPIO_PinState low = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4);
+	GPIO_PinState mid = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5);
+	GPIO_PinState high = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6);
+	GPIO_PinState too_high = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7);
+
     uint8_t new_status;
 	for (uint8_t i = 0; i < 32; i++) {
         new_status = ReadNote(i);
-        if (!status[i] && new_status) note_on(0, 41 + i, 60);
-        if (status[i] && !new_status) note_off(0, 41 + i, 0);
+        if (!status[i] && new_status) {
+        	if (low) note_on(0, 29 + i, 60);
+        	if (mid) note_on(0, 41 + i, 60);
+        	if (high) note_on(0, 53 + i, 60);
+        	if (too_high) note_on(0, 65 + i, 60);
+        }
+        if (status[i] && !new_status) {
+        	if (low) note_off(0, 29 + i, 0);
+        	if (mid) note_off(0, 41 + i, 0);
+        	if (high) note_off(0, 53 + i, 0);
+        	if (too_high) note_off(0, 65 + i, 0);
+        }
         status[i] = new_status;
     }
 }
