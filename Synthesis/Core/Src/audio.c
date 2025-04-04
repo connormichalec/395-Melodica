52826bbaa3701c4661d96be92022769e48f67f53
Connor Michalec <connormichalec@gmail.com>
2025-04-04 11:00:13 -0500

    synthesis progress, sawtooth wave, switched to a circular buffer

--- Synthesis/Core/Src/audio.c
+++ Synthesis/Core/Src/audio.c
@@ -16,10 +16,8 @@
 //USEFUL:
 //https://trebledj.me/posts/digital-audio-synthesis-for-dummies-part-3/
 
-uint16_t buffers[2][BUFFER_SIZE]; // Double buffer
-volatile uint8_t current_buffer = 0;
-volatile uint8_t buffer_ready = 0;
-uint32_t t = 0;
+uint16_t buffer[BUFFER_SIZE];	// For ring buffer
+uint32_t t = 0;					// Timestep, each count represents another sample.
 
 // Provided from system
 void * S_htim8;
@@ -35,30 +33,34 @@ void audio_signal_init(void * _htim8, void * _hdac) {
 	HAL_DAC_Start(_hdac, DAC_CHANNEL_1);
 }
 
-void fill_buffer(uint16_t *buffer) {
-    for (int i = 0; i < BUFFER_SIZE; i++, t++) {
-        float val = 0.2 * sinf(2 * M_PI * FREQUENCY * t / SAMPLE_RATE);
+void fill_buffer(uint16_t *buffer, long start, long end) {
+	float val;
+	// This is the duration each increment will represent.
+	float sawtooth_normalization = 1/(SAMPLE_RATE/FREQUENCY);
+
+    for (int i = start; i < end; i++, t++) {
+        //val = sinf(2 * M_PI * t* (FREQUENCY/SAMPLE_RATE));
+    	val = ((t)%(SAMPLE_RATE/FREQUENCY))*sawtooth_normalization;
         buffer[i] = (uint16_t)(2047 * val + 2048); // Centered properly
     }
 }
 
-void HAL_DAC_ConvHalfCpltCallbackCh1(DAC_HandleTypeDef *hdac) {
-    fill_buffer(buffers[current_buffer]);
-}
-
+// Fill second half of buffer after first one starts playing
 void HAL_DAC_ConvCpltCallbackCh1(DAC_HandleTypeDef *hdac) {
-    current_buffer = !current_buffer;
-    fill_buffer(buffers[current_buffer]);
+    fill_buffer(buffer, 0, BUFFER_SIZE/2);
 }
 
+// Fill first half of buffer after second half starts playing
+void HAL_DAC_ConvHalfCpltCallbackCh1(DAC_HandleTypeDef *hdac) {
+	fill_buffer(buffer, BUFFER_SIZE/2, BUFFER_SIZE);
+}
 
 void audio_signal_loop() {
-    // Fill both buffers initially
-    fill_buffer(buffers[0]);
-    fill_buffer(buffers[1]);
+    // Fill first half of buffer initially
+    fill_buffer(buffer, 0, BUFFER_SIZE/2);
 
     // Start DMA with Circular Mode
-    HAL_DAC_Start_DMA(S_hdac, DAC_CHANNEL_1, (uint32_t*)buffers[0], BUFFER_SIZE, DAC_ALIGN_12B_R);
+    HAL_DAC_Start_DMA(S_hdac, DAC_CHANNEL_1, (uint32_t*)buffer, BUFFER_SIZE, DAC_ALIGN_12B_R);
 
     while (1) {
         // Nothing needed here—DMA & callbacks handle real-time updates
